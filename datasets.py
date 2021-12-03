f649563d51f22ab8e309204dd392f98ec720edbf
Yassir <yassir.bendou@gmail.com>
2021-12-03 11:54:26 +0100

    adding tieredImagenet dataset

--- datasets.py
+++ datasets.py
@@ -328,6 +328,51 @@ def miniImageNet(use_hd = True):
     test_loader = iterator(datasets["test"][0], datasets["test"][1], transforms = all_transforms, forcecpu = True, shuffle = False, use_hd = use_hd)
     return (train_loader, train_clean, val_loader, test_loader), [3, 84, 84], (64, 16, 20, 600), True, False
 
+def tieredImageNet(use_hd=True):
+    """
+    tiredImagenet dataset
+    Number of classes : 
+    - train: 351
+    - val  : 97
+    - novel: 160
+    Number of samples per class: 1300
+    Total number of images: 790400
+    Images size : 84x84
+    """
+    datasets = {}
+    total = 790400
+    count = 0
+    for subset in ['train', 'val', 'test']:
+        data = []
+        target = []
+        subset_path = os.path.join(args.dataset_path, 'tieredimagenet', subset)
+        classe_files = os.listdir(subset_path)
+
+        for c, classe in enumerate(classe_files):
+            files = os.listdir(os.path.join(subset_path, classe))
+            for file in files:
+                count += 1
+                target.append(c)
+                path = os.path.join(subset_path, classe, file)
+                if not use_hd:
+                    image = transforms.ToTensor()(np.array(Image.open(path).convert('RGB')))
+                    data.append(image)
+                else:
+                    data.append(path)
+        datasets[subset] = [data, torch.LongTensor(target)]
+    print()
+    norm = transforms.Normalize(np.array([x / 255.0 for x in [125.3, 123.0, 113.9]]), np.array([x / 255.0 for x in [63.0, 62.1, 66.7]]))
+    train_transforms = torch.nn.Sequential(transforms.RandomResizedCrop(84), transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4), transforms.RandomHorizontalFlip(), norm)
+    all_transforms = torch.nn.Sequential(transforms.Resize(92), transforms.CenterCrop(84), norm)
+    if args.episodic:
+        train_loader = episodic_iterator(datasets["train"][0], 351, transforms = train_transforms, forcecpu = True, use_hd = True)
+    else:
+        train_loader = iterator(datasets["train"][0], datasets["train"][1], transforms = train_transforms, forcecpu = True, use_hd = use_hd)
+    train_clean = iterator(datasets["train"][0], datasets["train"][1], transforms = all_transforms, forcecpu = True, shuffle = False, use_hd = use_hd)
+    val_loader = iterator(datasets["val"][0], datasets["val"][1], transforms = all_transforms, forcecpu = True, shuffle = False, use_hd = use_hd)
+    test_loader = iterator(datasets["test"][0], datasets["test"][1], transforms = all_transforms, forcecpu = True, shuffle = False, use_hd = use_hd)
+    return (train_loader, train_clean, val_loader, test_loader), [3, 84, 84], (351, 97, 160, 1300), True, False
+
 import pickle
 
 def CUBfs():
@@ -459,6 +504,8 @@ def get_dataset(dataset_name):
         return CUBfs()
     elif dataset_name.lower() == "omniglotfs":
         return omniglotfs()
+    elif dataset_name.lower() == "tieredimagenet":
+        return tieredImageNet()
     else:
         print("Unknown dataset!")
 
