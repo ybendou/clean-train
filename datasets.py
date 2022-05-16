5cf86fb90cad5b7e4356d094a7fb580aaed204fa
Yassir <yassir.bendou@gmail.com>
2022-05-16 16:11:43 +0200

    small modif

--- datasets.py
+++ datasets.py
@@ -499,16 +499,18 @@ def imageNet(use_hd=True):
     all_transforms = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
-        transforms.ToTensor(),
+        transforms.ToTensor(), 
         norm,
     ])
 
     train_dataset = myImagenetDataset(os.path.join(args.dataset_path,'imagenet'), split='train', transform=all_transforms, closest_crops=closest_crops)
 
     train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=args.batch_size, shuffle=True,num_workers= min(8, os.cpu_count()))
+        train_dataset, batch_size=args.batch_size, shuffle=True,num_workers= min(8, os.cpu_count()), pin_memory=True)
 
     test_dataset = myImagenetDataset(os.path.join(args.dataset_path,'imagenet'), split='val', transform=all_transforms)
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=args.batch_size, shuffle=False,num_workers= min(8, os.cpu_count()), pin_memory=True)
 
     return (train_loader, train_loader, test_loader), [3, 224, 224], 1000, False, True
 
